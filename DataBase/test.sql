d8d9c9399454c60676dbd5a4d8b5ab118feb859e
jhorth98 <73620813+jhorth98@users.noreply.github.com>
2020-11-03 21:05:57 -0500

    Update test.sql
    prueba comentario

--- DataBase/test.sql
+++ DataBase/test.sql
@@ -1,30 +1,15 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 04-11-2020 a las 01:21:56
--- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.4.10
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `test`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `menjase`
+-- Estructura de tabla para la tablas `menjase` prueba
 --
 
 CREATE TABLE `menjase` (
